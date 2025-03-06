ab2599a191e94d0d4a5d4f0fa4cecd25be55d5d8
Shane Nixon <wavemorph@gmail.com>
2025-03-05 22:31:40 -0500

    Add files via upload

---
+++ Hide and Go seek timer.py
@@ -0,0 +1,12 @@
+import time
+
+# Write a for loop that counts to five.
+    # Body of the loop - print the loop iteration number and the word "Mississippi".
+    # Body of the loop - use: time.sleep(1)
+
+#loop iterates every 1 second. change "time.sleep" to use different times
+for i in range(1, 6):
+    print(i, "Mississippi")
+    time.sleep(1)
+# Write a print function with the final message.
+print("Ready or not,\nhere I come!")
